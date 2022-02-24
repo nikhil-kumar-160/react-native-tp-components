80562ebc3e6345d05c97a2d4e51acdbd84a5c061
Nikhil Kumar Choudhary <nikhil.kumar@mobcoder.com>
2022-02-24 15:21:36 +0530

    type declaration file added

---
+++ src/index.d.ts
@@ -0,0 +1,5 @@
+export function ClaimsList(): void;
+export function MessageList(): void;
+export function LoadingState(): void;
+export function FlightCard(): void;
+export function FlightOptions(): void;
